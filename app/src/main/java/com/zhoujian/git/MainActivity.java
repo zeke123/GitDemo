876a219db4b24564c1f1bb5ca38bc4122909cf26
zhoujian <1064791247@qq.com>
2017-02-10 10:59:00 +0800

    zeke  首次提交

--- app/src/main/java/com/zhoujian/git/MainActivity.java
+++ app/src/main/java/com/zhoujian/git/MainActivity.java
@@ -2,13 +2,11 @@ package com.zhoujian.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
